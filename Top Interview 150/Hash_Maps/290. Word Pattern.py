34a93dcf5f79ca0010bc1144f6ec184d461b446e
Pawanbisht491 <pawansinghbisht491@gmail.com>
2025-06-19 13:06:11 +0530

    Modified 290.

--- Top Interview 150/Hash_Maps/290. Word Pattern.py
+++ Top Interview 150/Hash_Maps/290. Word Pattern.py
@@ -1,12 +1,11 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        p = list(pattern)
         s = s.split()
-        if len(s) != len(p):
+        if len(s) != len(pattern):
             return False
 
         mapPS, mapSP = {}, {}
-        for c1, c2 in zip(p, s):
+        for c1, c2 in zip(pattern, s):
             if ((c1 in mapPS and mapPS[c1] != c2) or (c2 in mapSP and mapSP[c2] != c1)):
                 return False
             mapPS[c1] = c2
