01045fd172c1697cf0ec1833f5282cd0eb1c56ef
Pawanbisht491 <pawansinghbisht491@gmail.com>
2025-08-10 19:32:45 +0530

    Linked_List_Cycle Update

--- Top Interview 150/Linked_list/Linked_list_Cycle.py
+++ Top Interview 150/Linked_list/Linked_list_Cycle.py
@@ -1,3 +1,4 @@
+# 1st Solution
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         curr = head
@@ -8,3 +9,15 @@ class Solution:
             seen.add(curr)
             curr = curr.next
         return False
+    
+# 2nd Solution
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+            return False
